2255a743daaec1d1f3e268b770360075ea6641cf
epulonzo <saifulaqil@gmail.com>
2026-06-13 17:29:46 +0800

    test suit auth

---
+++ Test Suites/Authentication.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Authentication</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>20f91b22-9938-42cf-92e9-1508da077994</testSuiteGuid>
+</TestSuiteEntity>
