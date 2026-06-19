e2d61583fda922ced9ebb10fc9e6f929b1391401
epulonzo <saifulaqil@gmail.com>
2026-06-20 02:20:21 +0800

    report authentication

--- Test Suites/Authentication.ts
+++ Test Suites/Authentication.ts
@@ -11,4 +11,25 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>20f91b22-9938-42cf-92e9-1508da077994</testSuiteGuid>
+   <testCaseLink>
+      <guid>fb2c37b7-404d-419d-bfa7-95f237342007</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Invalid Credentials</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2ce57eb1-b639-4c15-ad3e-a8b82b04a807</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Successful Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f4d49531-0e90-4627-923f-d63ef7d6cb8e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Successful Registration</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
